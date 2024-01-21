a053c2f698ad17a783d6fde68ad50d711dbd19be
Daniel <73074657+danielazizyan@users.noreply.github.com>
2024-01-21 18:34:50 +0400

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,61 @@
+INSERT INTO Members VALUES ('Taylor', 'Swift');
+INSERT INTO Members VALUES ('Ed', 'Sheeran');
+INSERT INTO Members VALUES ('Ariana', 'Grande');
+INSERT INTO Members VALUES ('Leonardo', 'DiCaprio');
+INSERT INTO Members VALUES ('Adele', 'Adkins');
+INSERT INTO Members VALUES ('Dwayne', 'Johnson');
+INSERT INTO Members VALUES ('Beyoncé', 'Knowles');
+INSERT INTO Members VALUES ('Brad', 'Pitt');
+INSERT INTO Members VALUES ('Rihanna', 'Fenty');
+INSERT INTO Members VALUES ('Will', 'Smith');
+INSERT INTO Members VALUES ('Selena', 'Gomez');
+INSERT INTO Members VALUES ('Dylan', 'O''Brien');
+INSERT INTO Members VALUES ('Lady', 'Gaga');
+INSERT INTO Members VALUES ('Chris', 'Hemsworth');
+INSERT INTO Members VALUES ('Jennifer', 'Lopez');
+INSERT INTO Members VALUES ('Chris', 'Evans');
+INSERT INTO Members VALUES ('Katy', 'Perry');
+INSERT INTO Members VALUES ('Tom', 'Hanks');
+INSERT INTO Members VALUES ('Justin', 'Bieber');
+INSERT INTO Members VALUES ('Emma', 'Watson');
+INSERT INTO Members VALUES ('Hugh', 'Jackman');
+INSERT INTO Members VALUES ('Scarlett', 'Johansson');
+INSERT INTO Members VALUES ('Chris', 'Pratt');
+INSERT INTO Members VALUES ('Shakira', 'Mebarak'); 
+INSERT INTO Members VALUES ('Chris', 'Martin');
+INSERT INTO Members VALUES ('Jennifer', 'Lawrence');
+
+
+
+INSERT INTO Students VALUES (1, 'Taylor', 'Swift', '1989-12-13');
+INSERT INTO Students VALUES (2, 'Ed', 'Sheeran', '1991-02-17');
+INSERT INTO Students VALUES (4, 'Leonardo', 'DiCaprio', '1974-11-11');
+INSERT INTO Students VALUES (5, 'Adele', 'Adkins', '1988-05-05');
+INSERT INTO Students VALUES (6, 'Dwayne', 'Johnson', '1972-05-02');
+INSERT INTO Students VALUES (7, 'Beyoncé', 'Knowles', '1981-09-04');
+INSERT INTO Students VALUES (8, 'Brad', 'Pitt', '1963-12-18');
+INSERT INTO Students VALUES (9, 'Rihanna', 'Fenty', '1988-02-20');
+INSERT INTO Students VALUES (12, 'Dylan', 'O''Brien', '1991-08-26');
+INSERT INTO Students VALUES (15, 'Jennifer', 'Lopez', '1969-07-24');
+INSERT INTO Students VALUES (16, 'Chris', 'Evans', '1981-06-13'); 
+INSERT INTO Students VALUES (17, 'Katy', 'Perry', '1984-10-25');
+INSERT INTO Students VALUES (19, 'Justin', 'Bieber', '1994-03-01');
+INSERT INTO Students VALUES (20, 'Emma', 'Watson', '1990-04-15');
+INSERT INTO Students VALUES (21, 'Hugh', 'Jackman', '1968-10-12');
+INSERT INTO Students VALUES (22, 'Scarlett', 'Johansson', '1984-11-22');
+INSERT INTO Students VALUES (23, 'Chris', 'Pratt', '1979-06-21');
+INSERT INTO Students VALUES (24, 'Shakira', 'Mebarak', '1977-02-02');
+INSERT INTO Students VALUES (25, 'Chris', 'Martin', '1977-03-02');
+INSERT INTO Students VALUES (26, 'Jennifer', 'Lawrence', '1990-08-15');
+
+
+
+INSERT INTO Subjects VALUES('Physics', 6, 18);
+INSERT INTO Subjects VALUES('Project S1', 3, NULL);
+INSERT INTO Subjects VALUES('Applied Statistics', 3, 3);
+INSERT INTO Subjects VALUES('Discrete Math', 3, 10);
+INSERT INTO Subjects VALUES('C Programming', 3, 13);
+INSERT INTO Subjects VALUES('Computer Science', 3, 14);
+INSERT INTO Subjects VALUES('Management', 3, 11);
+INSERT INTO Subjects VALUES('French', 4, NULL);
+INSERT INTO Subjects VALUES('English', 2, NULL);
